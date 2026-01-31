dda37125a3c6778a012bbba03852994ce6b39d6f
tlima1011 <thiagolima1011@gmail.com>
2026-01-31 12:07:26 -0300

    Criado Classe Aluno

---
+++ src/br/com/educacao_uam_lw_ebook4/modelo/Aluno.java
@@ -0,0 +1,30 @@
+package br.com.educacao_uam_lw_ebook4.modelo;
+
+public class Aluno extends Pessoa {
+	
+	public String matricula; 
+	
+	public Aluno(String n) {
+		super(n);
+	}
+	
+	public Aluno(String n, String c) {
+		super(n, c);
+	}
+	
+	public void listar_frequencia() {
+		System.out.println("Frequencia Computada!");
+	}
+	
+	public void listar_notas() {
+		System.out.println("Nota Listada");
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [matricula=" + matricula + ", nome=" + nome + ", cpf=" + cpf + ", endereco=" + endereco
+				+ ", telefone=" + telefone + ", email=" + email + "]";
+	}
+	
+	
+}
